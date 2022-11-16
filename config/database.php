4ddd981944298b29a04609aad6f5f38a35e15bca
JaydenOK <603480498@qq.com>
2022-11-16 17:22:05 +0800

    修改表结构

--- config/database.php
+++ config/database.php
@@ -4,9 +4,9 @@
  */
 return [
     'host' => '192.168.92.209',
-    'user' => 'appuser',
-    'password' => 'adf2FASFAS',
-    'dbname' => 'yibai_account_system',
+    'user' => 'root',
+    'password' => 'root',
+    'dbname' => 't_account_system',
     'port' => '3306',
     'charset' => 'utf8',
 ];
