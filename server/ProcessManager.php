6f9e2455b3da6fcd9eb744dab6f0cebce0542abd
JaydenOK <603480498@qq.com>
2023-02-03 14:49:26 +0800

    clean up code

--- server/ProcessManager.php
+++ server/ProcessManager.php
@@ -2,6 +2,8 @@
 /**
  * 多进程消费者管理实例
  * 功能 : 使用多进程，启动多个rabbitMQ消费者，消费队列数据
+ *
+ * @author https://github.com/JaydenOK
  */
 
 namespace module\server;
@@ -10,6 +12,7 @@ use Exception;
 use module\lib\PdoClient;
 use module\lib\RabbitMQClient;
 use Swoole\Process;
+use Swoole\Timer;
 
 class ProcessManager
 {
@@ -130,7 +133,7 @@ class ProcessManager
         //默认定时器在执行回调函数时会自动创建协程，可单独设置定时器关闭协程。
         //第四个参数，是否在 callback function 中启用协程，开启后可以直接在子进程的函数中使用协程 API
         //蜕变为守护进程时，该进程的 PID 将发生变化，可以使用 getmypid() 来获取当前的 PID
-        \Swoole\Process::daemon();
+        Process::daemon();
         $this->pid = getmypid();
         //当前进程重命名
         $this->renameProcessName($this->master);
@@ -173,7 +176,7 @@ class ProcessManager
         $this->resumePidFromPidFile();
         //检测父进程并发送停止信号
         //$signo=0，可以检测进程是否存在，不会发送信号
-        if (\Swoole\Process::kill($this->pid, 0)) {
+        if (Process::kill($this->pid, 0)) {
             if (empty($this->pid)) {
                 return;
             }
@@ -181,12 +184,12 @@ class ProcessManager
             $this->writeLog('send pid stop:' . $this->pid);
             //Swoole\Timer::clear 不能用于清除其他进程的定时器，只作用于当前进程
             //向进程发送信号
-            \Swoole\Process::kill($this->pid, SIGTERM);
+            Process::kill($this->pid, SIGTERM);
         } else {
             //不存在，清理子进程
             foreach ($this->childPid as $childPid) {
                 $this->writeLog('pid not exist, stop child pid');
-                \Swoole\Process::kill($childPid, SIGTERM);
+                Process::kill($childPid, SIGTERM);
             }
         }
         $query = (new PdoClient())->getQuery();
@@ -248,35 +251,35 @@ class ProcessManager
     private function registerSignal()
     {
         //SIGTERM 终止，Termination
-        \Swoole\Process::signal(SIGTERM, function ($sig) {
+        Process::signal(SIGTERM, function ($sig) {
             //注册接收父进程终止信号，kill命令（此时，父进程尚未终止）
             $this->stopChildProcess($sig);
         });
         //SIGKILL 杀死
-        \Swoole\Process::signal(SIGKILL, function ($sig) {
+        Process::signal(SIGKILL, function ($sig) {
             //注册接收父进程强制终止信号，kill -9 命令
             $this->stopChildProcess($sig);
         });
         //SIGUSR1
-        \Swoole\Process::signal(SIGUSR1, function ($sig) {
+        Process::signal(SIGUSR1, function ($sig) {
             $this->writeLog("receive signal: {$sig}");
         });
         //SIGUSR2
-        \Swoole\Process::signal(SIGUSR2, function ($sig) {
+        Process::signal(SIGUSR2, function ($sig) {
             $this->writeLog("receive signal: {$sig}");
         });
         //SIGCHLD 子进程状态改变
         //每个子进程结束后，父进程必须都要执行一次 wait() 进行回收，否则子进程会变成僵尸进程，会浪费操作系统的进程资源。
         //如果父进程有其他任务要做，没法阻塞 wait 在那里，父进程必须注册信号 SIGCHLD 对退出的进程执行 wait。
         //SIGCHILD 信号发生时可能同时有多个子进程退出；必须将 wait() 设置为非阻塞，循环执行 wait 直到返回 false。
-        \Swoole\Process::signal(SIGCHLD, function ($sig) {
+        Process::signal(SIGCHLD, function ($sig) {
             /**
              * wait(false)，非阻塞模式
              * 阻塞等待子进程退出，并回收
              * 成功返回一个数组包含子进程的PID和退出状态码
              * 如 array('code' => 0, 'pid' => 15001)，失败返回false
              */
-            while ($ret = \Swoole\Process::wait(false)) {
+            while ($ret = Process::wait(false)) {
                 $this->writeLog("wait child {$sig}: {$ret['pid']}");
             }
         });
@@ -290,8 +293,8 @@ class ProcessManager
     public function stopChildProcess($sig, $masterExit = true)
     {
         foreach ($this->childPid as $childPid) {
-            if ($childPid && \Swoole\Process::kill($childPid, 0)) {
-                \Swoole\Process::kill($childPid, $sig);
+            if ($childPid && Process::kill($childPid, 0)) {
+                Process::kill($childPid, $sig);
                 $this->writeLog("child exit:{$childPid}");
             }
         }
@@ -310,7 +313,7 @@ class ProcessManager
     {
         //让父进程变成常驻进程，或为守护进程: \Swoole\Process::daemon(true);
         //注册的信号不再作为维持事件循环的条件，如程序只注册了信号而未进行其他工作将被视为空闲并随即退出 （此时可通过注册一个定时器防止进程退出）
-        $this->timerId = \Swoole\Timer::tick(60 * 1000, function ($timerId) {
+        $this->timerId = Timer::tick(60 * 1000, function ($timerId) {
         });
     }
 
@@ -422,7 +425,7 @@ class ProcessManager
             curl_setopt($ch, CURLOPT_COOKIE, $cookie);
         }
         $res = curl_exec($ch);
-        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        //$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
         return $res;
     }
